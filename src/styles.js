00bfc016424e168191680445b373f328f04906ac
Animesh Maiti <a.maiti770@gmail.com>
2023-05-15 22:26:12 +0530

    update

--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,5 @@
 const styles = {
+    // t1: "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
     paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
