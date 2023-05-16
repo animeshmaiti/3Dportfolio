5ae8add48635c0c829aa6f0e693478158805aaf4
Animesh Maiti <a.maiti770@gmail.com>
2023-05-16 17:41:28 +0530

    experience section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,7 +36,7 @@ const ServiceCard = ({index,title,icon}) => (
 )
 const About = () => {
   return (
-    <div>
+    <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
@@ -58,7 +58,7 @@ const About = () => {
           <ServiceCard key={service.title} index={index} {...service}/>
         ))}
        </div>
-    </div>
+    </>
   )
 }
 
